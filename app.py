aabe3062d2f9371bc7febc05f4745c51e7c13b89
Anna <goodarzi.anna@gmail.com>
2020-02-29 22:50:53 +0600

    Each theatre has their own url with a movies list

--- app.py
+++ app.py
@@ -3,27 +3,48 @@ import json
 
 app = Flask(__name__)
 
+schedule = {
+    "Bekmambetov Cinema" : {
+        "id": "1",
+        "movies": [
+            {
+                "name": "Девятое", 
+                "time": "11:40"
+            }
+        ]
+    }
+}
+
 
 @app.route('/')
 def hello():
+    with open('theatres.json') as f:
+        theatres = json.load(f)
+        result = {}
+    for id, name in theatres.items():
+        result[name] = id
     with open('db.json') as f:
         movies = json.load(f)
-    theatres = {}
+    schedule = {}
     for movie in movies:
         theatre = movie['theatre']
-        if theatre not in theatres:
-            theatres[theatre] = []
-        theatres[theatre].append(
+        if theatre not in schedule:
+            schedule[theatre] = {
+                "movies": [],
+                "id": result[theatre]
+            }
+        schedule[theatre]["movies"].append(
             {
                 'name': movie['name'],
                 'time': movie['time']
             }
         )
-    return render_template("index.html", theatres=theatres)
+
+    return render_template("index.html", schedule=schedule)
 
 
 @app.route('/cinema/<cinema_id>')
-def show_post(cinema_id):
+def cinema(cinema_id):
     with open('db.json') as f:
         movies = json.load(f)
     with open('theatres.json') as f:
